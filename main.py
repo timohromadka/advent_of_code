972b4fd345d78d8dd3894f6b884db7ad4d6ce260
Timo Hromadka <timo.hromadka@gmail.com>
2022-12-02 16:44:03 +0000

    Day 2, move txts to inputs dir

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--part', help='Select which part of the puzzle to run', type=int)
     args = parser.parse_args()
 
-    with open(f'{args.number}.txt') as f:
+    with open(f'inputs/{args.number}.txt') as f:
 
         puzzle = getattr(puzzles, f'puzzle{args.number}_{args.part}')
         print(puzzle(f))
--- puzzles.py
+++ puzzles.py
@@ -1,6 +1,26 @@
+"""
+For each subproblem, the TextWrapper object of the .txt file is passed
+"""
+
 def puzzle1_1(f):
     return max([sum([int(cal) for cal in cals.split("\n") if cal != '']) for cals in f.read().split("\n\n")])
 
 def puzzle1_2(f):
     sums = [sum([int(cal) for cal in cals.split("\n") if cal != '']) for cals in f.read().split("\n\n")]
     return sum(sorted(sums, reverse=True)[:3])
+
+def puzzle2_1(f):
+    left = ('A','B','C') # rock paper scissors
+    right = ('X','Y','Z') # rock paper scissors
+    scores = (3,6,0) # draw win loss
+    points = (1,2,3) # rock paper scissors
+    pairs = f.read().split("\n")
+    return sum(scores[(right.index(r) - left.index(l)) % 3] + points[right.index(r)] for l,_,r in pairs)
+
+def puzzle2_2(f):
+    left = ('A','B','C') # rock paper scissors
+    right = ('X','Y','Z') # loss draw win
+    scores = (0,3,6) # loss draw win
+    points = (1,2,3) # rock paper scissors
+    pairs = f.read().split("\n")
+    return sum(scores[right.index(r)] + points[(left.index(l) + right.index(r) - 1) % 3] for l,_,r in pairs)
