adb64801d38dc9ce39eeb25f1124372965082a12
Timo Hromadka <timo.hromadka@gmail.com>
2022-12-03 10:55:00 +0000

    Day 3

--- puzzles.py
+++ puzzles.py
@@ -24,3 +24,21 @@ def puzzle2_2(f):
     points = (1,2,3) # rock paper scissors
     pairs = f.read().split("\n")
     return sum(scores[right.index(r)] + points[(left.index(l) + right.index(r) - 1) % 3] for l,_,r in pairs)
+
+def puzzle3_1(f):
+    pairs = f.read().split("\n")
+    splits = [(pair[0:len(pair)//2], pair[len(pair)//2:]) for pair in pairs]
+    comms = [''.join(set(s1).intersection(s2)) for s1,s2 in splits]
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alph_dict = dict(map(reversed, dict(enumerate(alphabet, start=1)).items())) 
+    return sum(alph_dict.get(letter) for letter in comms)
+
+def puzzle3_2(f):
+    pairs = f.read().split("\n")
+    n = 3
+    splits = [pairs[i:i+n] for i in range(0, len(pairs), n)]
+    comms = [set(a) & set(b) & set(c) for a,b,c in splits]
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alph_dict = dict(map(reversed, dict(enumerate(alphabet, start=1)).items())) 
+    return sum(alph_dict.get(num) for num,*_ in comms)
+
